4dc2a116a5df679d74fd137aff404f0aebd60eca
KIRISHIMA Raitehu <raitehu@pleiades-union.local>
2023-06-10 15:19:05 +0900

    Route 53の設定完了

--- main.tf
+++ main.tf
@@ -27,6 +27,30 @@ resource "aws_security_group" "default" {
   egress  = []
 }
 
+# Route 53
+# Kongoh
+resource "aws_route53_zone" "kongoh" {
+  name = "kongoh.xyz"
+}
+resource "aws_route53_record" "kongoh_A" {
+  zone_id = aws_route53_zone.kongoh.zone_id
+  name    = "*.kongoh.xyz"
+  type    = "A"
+  ttl     = "300"
+  records = [var.on_premises_ip]
+}
+# pleiades-union
+resource "aws_route53_zone" "pleiades" {
+  name = "pleiades-union.com"
+}
+resource "aws_route53_record" "pleiades_union_A" {
+  zone_id = aws_route53_zone.pleiades.zone_id
+  name    = "*.pleiades-union.com"
+  type    = "A"
+  ttl     = "300"
+  records = [var.on_premises_ip]
+}
+
 resource "aws_s3_bucket" "terraform_backend" {
   bucket = "cloudgate-terraform-backend"
 }
