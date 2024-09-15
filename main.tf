03bd6676ddcf6e2b76e73de9bf7f31c4eb9016fb
SHIMOTORI Masanari <shimotori.masanari@gmail.com>
2024-09-16 02:38:21 +0900

    :recycle: リファクタ

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ module "s3_terraformbackend" {
   bucket = "cloudgate-terraform-backend"
 }
 
-#################
-#      VPC      #
-#################
+##################
+#     Common     #
+##################
 module "vpc" {
   source = "./modules/vpc"
 
@@ -19,102 +19,15 @@ module "vpc" {
   cidr_block_private       = local.cidr_block_private
   availability_zone_suffix = local.availability_zone_suffix
 }
-
 module "route53" {
   source = "./modules/route53"
 
   on_premises_ip = var.on_premises_ip
 }
+module "parameter_stores" {
+  source = "./modules/parameter_stores"
 
-# CloudFront
-resource "aws_cloudfront_distribution" "cloudgate" {
-  # Origin - yakan
-  origin {
-    domain_name = module.s3_yakan.bucket_domain_name
-    origin_id   = module.s3_yakan.bucket_id
-    s3_origin_config {
-      origin_access_identity = module.s3_yakan.OAI_access_identity_path
-    }
-  }
-  # Origin - flowerstand AR
-  origin {
-    domain_name = module.s3_flowerstand_ar.bucket_domain_name
-    origin_id   = module.s3_flowerstand_ar.bucket_id
-    s3_origin_config {
-      origin_access_identity = module.s3_flowerstand_ar.OAI_access_identity_path
-    }
-  }
-
-  # Origin - app ALB
-  # TODO
-
-  enabled             = true
-  default_root_object = "index.html"
-
-  ordered_cache_behavior {
-    path_pattern     = "/flowerstand-ar*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = module.s3_flowerstand_ar.bucket_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = module.s3_yakan.bucket_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  restrictions {
-    geo_restriction {
-      locations        = []
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-#################
-#   フラスタAR   #
-#################
-module "s3_flowerstand_ar" {
-  source = "./modules/s3_via_cloudfront"
-
-  bucket = "raitehu-flowerstand-ar"
-}
-module "flowerstand_ar_developers" {
-  source = "./modules/s3_upload_users"
-
-  user_names = [
-    "Tsurara",
-    "Pon"
-  ]
+  tags = local.wanderers_info.tags
 }
 
 #################
@@ -126,19 +39,9 @@ module "s3_yakan" {
   bucket = "yakan-static"
 }
 
-module "parameter_stores" {
-  source = "./modules/parameter_stores"
-
-  tags = local.wanderers_info.tags
-}
-
-module "wanderers_info_tools" {
-  source = "./modules/wanderers_info_tools"
-
-  tags                       = local.wanderers_info.tags
-  wanderers_info_backend_url = var.wanderers_info_backend_url
-}
-
+#################
+#      Apps     #
+#################
 module "app_network" {
   source = "./modules/app_network"
 
@@ -158,5 +61,43 @@ module "app_server" {
   security_group_ids = [
     module.vpc.sg_ssh_id,
     module.vpc.sg_elb_id
+  ]
+}
+
+#################
+#     Tools     #
+#################
+module "wanderers_info_tools" {
+  source = "./modules/wanderers_info_tools"
+
+  tags                       = local.wanderers_info.tags
+  wanderers_info_backend_url = var.wanderers_info_backend_url
+}
+
+#################
+#   フラスタAR   #
+#################
+# CloudFront
+module "flowerstand_ar_network" {
+  source = "./modules/flowerstand_ar_network"
+
+  s3_bucket_id_yakan                   = module.s3_yakan.bucket_id
+  s3_bucket_domain_name_yakan          = module.s3_yakan.bucket_domain_name
+  s3_OAI_path_yakan                    = module.s3_yakan.OAI_access_identity_path
+  s3_bucket_id_flowerstand_ar          = module.s3_flowerstand_ar.bucket_id
+  s3_bucket_domain_name_flowerstand_ar = module.s3_flowerstand_ar.bucket_domain_name
+  s3_OAI_path_flowerstand_ar           = module.s3_flowerstand_ar.OAI_access_identity_path
+}
+module "s3_flowerstand_ar" {
+  source = "./modules/s3_via_cloudfront"
+
+  bucket = "raitehu-flowerstand-ar"
+}
+module "flowerstand_ar_developers" {
+  source = "./modules/s3_upload_users"
+
+  user_names = [
+    "Tsurara",
+    "Pon"
   ]
 }
---
+++ modules/flowerstand_ar_network/input.tf
@@ -0,0 +1,21 @@
+# TODO: yakan系は別のCloudFrontへ移行する
+# yakan
+variable "s3_bucket_id_yakan" {
+  type = string
+}
+variable "s3_bucket_domain_name_yakan" {
+  type = string
+}
+variable "s3_OAI_path_yakan" {
+  type = string
+}
+# flowerstand
+variable "s3_bucket_id_flowerstand_ar" {
+  type = string
+}
+variable "s3_bucket_domain_name_flowerstand_ar" {
+  type = string
+}
+variable "s3_OAI_path_flowerstand_ar" {
+  type = string
+}
